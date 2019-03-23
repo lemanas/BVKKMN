ee4f339e9b8ddf10be323fdff1ae2c5c0763378b
VL <V.lastauskas@protonmail.com>
2019-03-23 12:35:47 +0200

    init

---
+++ BVKMN/FinancialFunctions.cs
@@ -0,0 +1,22 @@
+﻿using Excel.FinancialFunctions;
+
+namespace BVKMN
+{
+    static class FinancialFunctions
+    {
+        public static double Pmt(double interestRate, int months, double sum)
+        {
+            return Financial.Pmt(interestRate / 12, months, sum * -1, 0, PaymentDue.BeginningOfPeriod);
+        }
+
+        public static double Rate(int months, double pmt, double sum)
+        {
+            return Financial.Rate(months, pmt, sum * -1, 0, PaymentDue.BeginningOfPeriod);
+        }
+
+        public static double Effect(double rate)
+        {
+            return Financial.Effect(rate * 12, 12);
+        }
+    }
+}
---
+++ BVKMN/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BVKMN
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter sum of a loan:");
+            var sum = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter months:");
+            var months = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter interest rate (%):");
+            var interestRate = Convert.ToDouble(Console.ReadLine());
+
+            var pmt = FinancialFunctions.Pmt(interestRate / 100, months, sum);
+            var rate = FinancialFunctions.Rate(months, pmt, sum);
+            var effect = FinancialFunctions.Effect(rate);
+
+            Console.WriteLine($"BVKKMN: {effect * 100}");
+            Console.ReadKey();
+        }
+    }
+}
